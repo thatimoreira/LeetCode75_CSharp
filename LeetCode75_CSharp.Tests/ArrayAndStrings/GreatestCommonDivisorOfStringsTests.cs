54f20ab70c8b8c2c36227ab133be39f159ed6431
thatimoreira <thatimoreira.dev@gmail.com>
2026-04-26 20:08:07 -0300

    test(1071): add greatest common divisor of strings unit tests

---
+++ LeetCode75_CSharp.Tests/ArrayAndStrings/GreatestCommonDivisorOfStringsTests.cs
@@ -0,0 +1,32 @@
+using LeetCode75_CSharp.ArrayAndStrings;
+
+namespace LeetCode75_CSharp.Tests.ArrayAndStrings;
+
+public class GreatestCommonDivisorOfStringsTests
+{
+    [Theory]
+    [InlineData("TEST", "ABC")]
+    [InlineData("LEET", "CODE")]
+    [InlineData("AAAAAB", "AAA")]
+    public void ShouldReturnEmptyString_WhenStr2DoesNotDivideStr1(string str1, string str2)
+    {
+        var sb = new GreatestCommonDivisorOfStrings();
+        
+        var result = sb.GcdOfStrings(str1, str2);
+        
+        Assert.Equal("", result);
+    }
+    
+    [Theory]
+    [InlineData("ABCABCABC", "ABC", "ABC")]
+    [InlineData("ABABAB", "ABAB", "AB")]
+    [InlineData("ABC", "ABCABC", "ABC")]
+    public void ShouldReturnGcd_WhenStr2DividesStr1(string str1, string str2, string expected)
+    {
+        var sb = new GreatestCommonDivisorOfStrings();
+        
+        var result = sb.GcdOfStrings(str1, str2);
+        
+        Assert.Equal(expected, result);
+    }
+}
